f9a7b08d28dd6e7f9e0c9727baac15b7a9710076
Sanjay M <162327366+SanjayStd@users.noreply.github.com>
2024-04-26 21:51:20 +0530

    Add files via upload

---
+++ fn.js
@@ -0,0 +1,14 @@
+function myfunction()
+{
+  alert("Root found using Newton-Raphson method: ")
+}
+
+function info()
+{
+  alert("Information not updated yet")
+}
+
+function about()
+{
+  alert("A Capstone project made by the department of Mechanical Engineering.")
+}
